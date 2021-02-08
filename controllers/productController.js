31bda89ca23c37b049b2cff6c24af4ff3cbb58c4
SlaviManzurov <77936591+SlaviManzurov@users.noreply.github.com>
2021-02-08 14:04:22 +0200

    Create cube

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,8 @@
 const { Router } = require('express')
 const router = Router()
+const Cube = require('../models/cube')
+const uniqid = require('uniqid')
+
 
 router.get('/', (req, res) => {
     res.render('home')
@@ -9,13 +12,20 @@ router.get('/create', (req, res) => {
     res.render('create')
 })
 
-router.post('/create', (req,res) => {
-    console.log(req.body);
+router.post('/create', (req, res) => {
 
+    let data = req.body;
+    let cube = new Cube(uniqid(),
+        data.name,
+        data.description,
+        data.imageUrl,
+        data.difficultyLevel
+    )
+    console.log(cube);
     res.redirect('/')
 })
 
-router.get('/details/:productId',(req,res)=>{
+router.get('/details/:productId', (req, res) => {
     res.render('details')
 })
 
