d843c46e15d48834727f645b33bac1e70773997d
SlaviManzurov <77936591+SlaviManzurov@users.noreply.github.com>
2021-02-08 12:03:42 +0200

    Second Controller

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,9 @@
-const productController = require('../controllers/productController')
 // TODO: Require Controllers...
+const productController = require('../controllers/productController')
+const aboutController = require('../controllers/aboutController')
+
 
 module.exports = (app) => {
     app.use('/', productController)
+    app.use('/about', aboutController)
 };
